4ee9714489573c6b4b9f6df0dcc850922e0b8220
sublimebovine <87881310+sublimebovine@users.noreply.github.com>
2026-02-08 12:59:02 -0500

    Add files via upload

---
+++ nothread.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  8 16:15:41 2026
+
+@author: sbjoh
+"""
+
+import wave
+import os
+import time
+import sys
+#import Live
+from collections import deque
+from queue import Queue
+import pyaudio
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.animation as animation
+import random
+#from tkinter import * 
+import madmom
+#from numba import jit, cuda
+import threading
+FORMAT = pyaudio.paInt16
+CHANNELS = 2#set this to the channel number of recording device can be found using pyaudio commands
+RATE = 44100 #set this to the sampling rate of recording device can be found using pyaudio commands
+CHUNK = int(2048)
+q  = deque()
+bytes_q  = []
+try:
+    audio = pyaudio.PyAudio()
+except:
+    print("audio failure")
+    
+############################################
+def callback(in_data, frame_count, time_info,status):
+    bytes_q.append(in_data)
+    #data = np.frombuffer(in_data, dtype=np.int16)
+    #data = data.tolist()
+    #q.appendleft(data)
+    
+    if len([item for sublist in bytes_q for item in sublist])>250000:
+        #print(len([item for sublist in bytes_q for item in sublist]))
+        del bytes_q[0]
+    return None, pyaudio.paContinue
+stream = audio.open(
+        format=pyaudio.paInt16,
+			channels=2,
+			rate=RATE,
+			input=True,
+            output=False,
+            input_device_index=1,
+            output_device_index=3,
+			frames_per_buffer=CHUNK,
+			stream_callback=callback)
+feat_processor = madmom.features.chords.CNNChordFeatureProcessor()
+recog_processor = madmom.features.chords.CRFChordRecognitionProcessor()
+#@jit(target_backend='cuda')
+chordsls= deque() 
+def process(save):
+    feats = feat_processor(np.frombuffer(b''.join(save), dtype=np.int16)[::2])
+    chords = recog_processor(feats)
+    return(chords)
+
+threads=[]           
+while stream.is_active():
+    print("===========================================")
+    try:
+
+        #time.sleep(.01)
+        save=bytes_q
+        print(process(save))
+
+    except:
+        print("empty")
+audio.terminate()
