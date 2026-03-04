5ac3c1eee864f07d69681f4f5df6786b82efe02b
KashviC <kashvirc@gmail.com>
2026-03-04 11:35:01 -0500

    added the chord accuracy detection. needs to be tested w guitar

--- main_thread_new.py
+++ main_thread_new.py
@@ -1,17 +1,32 @@
 from enclosedlisteners import * 
+import send_data
 #import multiprocessing as mp
 #from multiprocessing import Process
-rate,refaudio=wavfile.read("riptide.wav")
+rate,refaudio=wavfile.read("C:/Users/kashv/Capstone_Group19/Capstone-Group-19/riptide.wav") #THIS WILL HAVE TO BE CHANGED ON UR SYSTEM
 feats = feat_processor(np.frombuffer(refaudio, dtype=np.int16))
 actual = recog_processor(feats)
 threads=[]     
+correct = True
+
 class record:
     chords=['n']
-    
+    actualchords =[]
     lt=datetime.now().timestamp()
     times=[lt]
     def tx(self,send):
         print("new chord sent: "+str(send))
+
+    def check(self,chordin):
+        chordin= "".join(send_data.cleanInput(chordin))
+        actualsingle = rec.actualchords[0]
+        if actualsingle == chordin: #correct
+            print("correct chord. moving on")
+            rec.actualchords.pop(0)
+            return True
+        else: 
+            print("incorrect chord")
+            return False
+
     def manage(self,chordsin):
         print("ao")
         ct=datetime.now().timestamp()
@@ -28,11 +43,14 @@ class record:
                 self.chords.append(i[2])
                 self.times.append(ct-self.lt)
             self.lt=ct
+
 if __name__ == '__main__':
+    from send_data import *
     rec=record()
+    rec.actualchords=send_data.cleanInput(actual)
     for i in range(1):
         #enclosedthread1()
-        threads.append(Process(target=enclosedthread, args=(chordsls,)))
+        threads.append(threading.Thread(target=enclosedthread, args=(chordsls,)))
         threads[i].start()
         time.sleep(.1)
     while True:
@@ -44,8 +62,15 @@ if __name__ == '__main__':
                     print(start_time)
                     newchord=(chordsls.get())
                     rec.manage(newchord)
-                    #print(sm)
-                    #print(np_array)
+                    result = rec.check(newchord)
+                    if result: #move onto next chord
+                        print("move to next")
+                        correct = True
+                    else: 
+                        print("try again")
+                        correct = False
+                    #---------------------------
+                    
     
         except:
             print("empty")
