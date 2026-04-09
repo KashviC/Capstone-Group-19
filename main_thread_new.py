83963ca3ef36cd7299e723a91e8459ccb3f9bd86
astrofiona <53826429+astrofiona@users.noreply.github.com>
2026-04-09 19:16:22 -0400

    Add files via upload

--- main_thread_new.py
+++ main_thread_new.py
@@ -1,64 +1,137 @@
-from enclosedlisteners import * 
-import serial
+from enclosedlisteners import *
+import send_data
 #import multiprocessing as mp
 #from multiprocessing import Process
-rate,refaudio=wavfile.read("riptide.wav")
-feats = feat_processor(np.frombuffer(refaudio, dtype=np.int16))
+rate, data = wavfile.read(("C:/Users/tisfi/Documents/capstone/riptide.wav"))
+new_rate = 44100
+new_samples = round(len(data) * new_rate / rate)
+# Resample
+new_data = (sps.resample(data, new_samples)).astype(np.int16).copy()
+feats = feat_processor(np.frombuffer(new_data, dtype=np.int16))
 actual = recog_processor(feats)
 threads=[]
-arduino = serial.Serial(port='COM5', baudrate=9600, timeout=.1) 
-def write_read(x): 
-    arduino.write() 
-    time.sleep(0.1) 
-    data = arduino.read(size=1)
-    print(data)
-    return data  
+span=3     
+correct = True
+breakfree = False
 
 class record:
+    span=3
     chords=['n']
-    
-    lt=datetime.now().timestamp()
-    times=[lt]
-    
+    actualchords =[]
+    chordSend = ''
+
+    lt=datetime.now().timestamp()-span
+    fixed=lt
+    times=[0]
+    timesstart=[0]
     def tx(self,send):
         print("new chord sent: "+str(send))
+    
+    def check(self,chordin):
+        chordin= "".join(send_data.cleanInput(chordin))
+        actualsingle = rec.actualchords[0]
+        if actualsingle == chordin: #correct
+            print("correct chord. moving on")
+            self.chordSend = rec.actualchords.pop(0)
+            return self.chordSend, True
+        else: 
+           # print("incorrect chord")
+            return actualsingle, False
 
     def manage(self,chordsin):
-        print("ao")
-        ct=datetime.now().timestamp()
-        print("ng")
+        #print("ao")
+        ct=chordsin[1].timestamp()-self.fixed
+        chordsin=chordsin[0]
+        #print(ct)
         for i in chordsin:
-            print(i[2])
-            print(self.chords[len(self.chords)-1])
-            if (i[2]==self.chords[len(self.chords)-1]):
-                self.times[len(chordsin)-1]=self.times[len(chordsin)-1]+float(ct-self.lt)
-                print(i[2])
-            else:
-                print(i)
-                self.tx(i)
+            #print(i[2])
+            #print(self.chords[len(self.chords)-1])
+            index=0
+            while((ct-(self.span-i[0]))<self.times[len(self.chords)-1-index]):
+                index+=1
+            if ((i[2]==self.chords[len(self.chords)-1] and index==1)):
+                if index==1:
+                    self.times[len(self.chords)-1]=ct-(self.span-i[1])
+                #print(i[2])
+            elif(index==0):
+                #print(i)
+                self.tx(i[2])
                 self.chords.append(i[2])
-                self.times.append(ct-self.lt)
-            self.lt=ct
+                self.times.append(ct-(self.span-i[1]))
+                self.timesstart.append(ct-(self.span-i[0]))
+        self.lt=ct
 if __name__ == '__main__':
+    from send_data import *
     rec=record()
+    send_data.initialize() #initialize the arduino connection 
+
+    rec.actualchords=send_data.cleanInput(actual)
+    rec.span=span
+    time.sleep(1)
+    #rec.manage([(0., 6.8, 'N')])
+    #rec.manage([(0,2,'N'),(2,6.8,'A')])
+    #rec.manage([(0,2,'N'),(2,6.8,'B')])
+    #rec.manage([(0,2,'C'),(2,6.8,'B')])
+    #rec.manage([(0,2,'N'),(2,6.8,'D')])
+
     for i in range(1):
         #enclosedthread1()
-        threads.append(threading.Thread(target=enclosedthread, args=(chordsls,)))
+        threads.append(threading.Thread(target=enclosedthread, args=(chordsls, 3)))
         threads[i].start()
         time.sleep(.1)
-
     while True:
-        time.sleep(.01)
+        if breakfree == True: break #SEE IF THIS ACTUALLY WORKS LOL I DIDNT TEST IT IM LAZY 
+        #time.sleep(.01)
         try:
             if chordsls.qsize()>0:
                 for i in range(chordsls.qsize()):
-                    start_time= datetime.now()
-                    print(start_time)
+                    # start_time= datetime.now()
+                    # print(start_time)
                     newchord=(chordsls.get())
+                    print("Chord detected:")
+                    print(newchord[0])
+                    # print("time")
+                    # print(newchord[1].timestamp())
                     rec.manage(newchord)
-                    #print(sm)
-                    #print(np_array)
-                    write_read(newchord)
+                    result = rec.check(newchord)
+                    #test all chords on LED display
+                    send_data.sendChord('Ab:maj')
+                    time.sleep(1)
+                    send_data.sendChord('F:min')
+                    time.sleep(1)
+                    send_data.sendChord('Eb:maj')
+                    time.sleep(1)
+                    send_data.sendChord('G:maj')
+                    time.sleep(1)
+                    send_data.sendChord('C:maj')
+                    time.sleep(1)
+                    send_data.sendChord('D:maj')
+                    time.sleep(1)
+                    send_data.sendChord('E:maj')
+                    time.sleep(1)
+                    send_data.sendChord('A:maj')
+                    time.sleep(1)
+                    send_data.sendChord('A:min')
+                    time.sleep(1)
+                    send_data.sendChord('E:min')
+                    time.sleep(1)
+                    send_data.sendChord('D:min')
+                    time.sleep(1)
+                    correct = True
+                    if result[1]: #move onto next chord
+                        print("move to next")
+                        send_data.sendChord(result[0])
+                        correct = True
+                    else: 
+                        print("try again")
+                        send_data.sendChord(result[0])
+                        correct = False
+                    if len(rec.actualchords) == 0: 
+                        print("DONE YOURE DONE UR FREE YAYYY")
+                        breakfree = True
+                        send_data.closePort()
+                        break
+    
         except:
             print("empty")
-audio.terminate()
+            audio.terminate()
---
+++ send_data.py
@@ -0,0 +1,80 @@
+'''
+The purpose of this file is to take the chords identified from main_thread and send 2 bytes to the arduino. 
+That number will be used in the arduino chord to match with the arduino's internal look up table we designed.
+
+'''
+from serial import Serial
+import time
+import re
+import datetime
+import serial.tools.list_ports
+
+#The 2nd param in this dict indicates which strings should be left open
+CHORDS = {
+    'Ab:maj':'1', 
+    'F:min':'2',
+    'Eb:maj':'3',
+    'G:maj':'4',
+    'C:maj':'5',
+    'D:maj':'6',
+    'E:maj':'7',
+    'A:maj':'8',
+    'A:min':'9',
+    'E:min':'10',
+    'D:min':'11'
+}
+serialinst = serial.Serial()
+portsList = []
+COM = 7 #change if needed 
+
+def initialize():
+    comports = serial.tools.list_ports.comports()
+    for port in comports: 
+        portsList.append(str(port)) #getallports 
+
+    avalport = None
+    for i in range(len(comports)):
+        if portsList[i].startswith("COM"+str(COM)): #if selected port is avaible
+            avalport = "COM"+str(COM)
+    if avalport is None: 
+        print("No ports available. Exiting")
+    else:
+        serialinst.baudrate = 9600
+        serialinst.port = avalport
+        serialinst.open()
+
+def closePort(): 
+    serialinst.close()
+
+
+def cleanInput(chordin): #isolate chords and clean it up
+    isolated_chords = []
+    if len(chordin) == 2 and isinstance(chordin[1], datetime.datetime): #check to see if this is the actual chords or input chord from guitar
+        isolated_chords.append(chordin[0][0][2]) #input chord from 
+    else:
+        for chord in chordin: 
+            if chord[2] != 'N':
+                isolated_chords.append(chord[2])
+    return isolated_chords
+		
+def sendChord(newchord): 
+    #sends the next chord
+    value = CHORDS.get(newchord)
+    if value is not None:
+        sendb = bytearray(value+'\n','ascii');
+        serialinst.write(sendb)
+        print('Sent chord')
+        print(newchord)
+        time.sleep(1)
+        print('Sent code over serial:')
+
+    else:
+        serialinst.write(bytearray('\n','ascii'))
+        print('No chord detected')
+        time.sleep(1)
+        print('Sent code over serial:')
+        
+def confirmChord():
+    serIn = serialinst.readline().decode('ascii')
+    print(serIn)
+
