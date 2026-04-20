1cddb2bbe3984763afca0ef3ede1834a519eaf9d
sublimebovine <87881310+sublimebovine@users.noreply.github.com>
2026-04-20 11:50:13 -0400

    Update enclosedlisteners.py

--- enclosedlisteners.py
+++ enclosedlisteners.py
@@ -1,17 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  8 19:06:00 2026
+
+@author: sbjoh
+"""
+import wave
+import os
+import time
+import sys
+#import Live
+from collections import deque
+from queue import Queue
+import pyaudio
+import numpy as np
+import random
+#from tkinter import * 
+import madmom
 #from numba import jit, cuda
 import threading
 from scipy.io import wavfile
 from datetime import datetime
 from madmom.audio.chroma import DeepChromaProcessor
+from madmom.audio.chroma import CLPChromaProcessor
+from madmom.audio.chroma import CLPChroma
 from scipy.io import wavfile
 import scipy.signal as sps
 
 # Read 48kHz file
-
+threshold=2
 #feat_processor = madmom.features.chords.CNNChordFeatureProcessor()
 #recog_processor = madmom.features.chords.CRFChordRecognitionProcessor()
-feat_processor=DeepChromaProcessor()
+feat_processor=DeepChromaProcessor(threshold=1)
+#feat_processor=CLPChromaProcessor(threshold=.2)
 recog_processor=madmom.features.chords.DeepChromaChordRecognitionProcessor(fps=20)
+#recog_processor=madmom.features.chords.CLPChromaProcessor(fps=20)
 FORMAT = pyaudio.paInt16
 CHANNELS = 2#set this to the channel number of recording device can be found using pyaudio commands
 RATE = 44100 #set this to the sampling rate of recording device can be found using pyaudio commands
@@ -65,11 +87,20 @@ def enclosedthread(chordsls, span):
 
     print("here")
     time.sleep(span)
+    feats=[0,-1]
+    threshold=np.zeros(12)
     while stream.is_active():
         save=bytes_q
         start_time= datetime.now()
+        #threshold=0#threshold*7/8+(np.max(feats)/1.2)/8
+        threshold=threshold*19/20+(np.max(feats,axis=0)/1.4)/20
+        #print(threshold)
         feats = feat_processor(np.frombuffer(b''.join(save), dtype=np.int16)[::2])
-        chords = recog_processor(feats)
+        #feats=CLPChroma(feats)
+        #print(feats)
+        #threshold=threshold*(threshold>.2)*1+.2*(threshold<.2)
+        #threshold=0
+        chords = recog_processor(feats*((feats>threshold)*1))
         chords=[chords,datetime.now()]
         #print( datetime.now() - end1)
         #print( chords)
@@ -88,7 +119,7 @@ def threadsim(chordsls):
     datalen=len(new_data)
     new_data=np.concatenate((new_data, np.zeros([400000,2]))).astype(np.int16)
     cs=2048
-    span=3
+    span=.5
     sampspan=span*44.1*1000
     bytes_q  = []
     for i in range(int(datalen/2048)-2):
@@ -99,7 +130,7 @@ def threadsim(chordsls):
         save=bytes_q
         start_time= datetime.now()
         feats = feat_processor(np.frombuffer(b''.join(save), dtype=np.int16)[::])
-        chords = recog_processor(feats)
+        chords = recog_processor(feats*(feats>.7))
         chords=[chords,datetime.now()]
         #print( datetime.now() - end1)
         #print( chords)
