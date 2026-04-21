f2c483e0e7d692638d2e320fd425b3efeeae4677
sublimebovine <87881310+sublimebovine@users.noreply.github.com>
2026-04-21 14:43:09 -0400

    Update combined.py

--- combined.py
+++ combined.py
@@ -2,13 +2,15 @@ from enclosedlisteners import *
 import send_data
 import multiprocessing as mp
 from multiprocessing import shared_memory, Process, Lock
-import intempomode as temp
+#import intempomode as temp
 import asyncio
+import threading
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 #import multiprocessing as mp
 #from multiprocessing import Process
 chordsls= mp.Queue()
-rate, data = wavfile.read(("C:/Users/kashv/Capstone_Group19/Capstone-Group-19/audios/SZA.wav"))
+rate, data = wavfile.read(("riptide.wav"))
 new_rate = 44100
 new_samples = round(len(data) * new_rate / rate)
 # Resample
@@ -16,11 +18,10 @@ new_data = (sps.resample(data, new_samples)).astype(np.int16).copy()
 feats = feat_processor(np.frombuffer(new_data, dtype=np.int16))
 actual = recog_processor(feats)
 threads=[]
-span=3     
+span=.3     
 actual=["C:maj","F:min","G:maj","A:min"]
 correct = True
 breakfree = False
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 loop = None
 
 
@@ -48,18 +49,25 @@ async def websocket_endpoint(websocket: WebSocket):
             print("Unity disconnected")
 
 async def sendUnity(chord): 
-    for client in connected:
-        try: 
-            await client.send(chord)
-        except:
-            pass
-
+    client =connected[0]
+    try: 
+        await client.send(chord)
+    except:
+        pass
+async def rxu(rec): 
+    client =connected[0]
+    try: 
+        while True:
+            data = await websocket.receive_text()
+            rec.advance()
+    except:
+        pass
 #send_data.closePort()
 
 class record:
-    span=3
+    span=.3
     chords=['n']
-    actualchords =[]
+    actualchords =["C:maj","F:min","G:maj","A:min"]
     chordSend = ''
 
     lt=datetime.now().timestamp()-span
@@ -73,20 +81,15 @@ class record:
         #test all chords on LED display
         loop = asyncio.get_event_loop()
         loop.create_task(sendUnity(result[0]))
-
-        #correct = True
-       # if result[1]: #move onto next chord
-        #    print("move to next")
-           # send_data.sendChord(result[0]) ------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
-         #   correct = True
-        #else: 
-            #print("try again")
-            #send_data.sendChord(result[0])
-       #     correct = False
-       # if len(rec.actualchords) == 0: 
-        #    print("DONE YOURE DONE UR FREE YAYYY")
-         #   breakfree = True
-            #send_data.closePort()------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
+    def advance(self):
+        correct = True
+        print("correct chord. moving on")
+        self.actualchords.pop(0)
+        self.chordSend = self.actualchords[0]
+        print("move to next")
+        send_data.sendChord(self.chordSend) #------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
+        correct = True
+        #send_data.closePort()------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
 
     def check(self,chordin):
         #chordin= "".join(send_data.cleanInput(chordin))
@@ -100,7 +103,7 @@ class record:
         else: 
             # print("incorrect chord")
             return actualsingle, False
-
+        
     def manage(self,chordsin):
         #print("ao")
         ct=chordsin[1].timestamp()-self.fixed
@@ -110,13 +113,17 @@ class record:
             #print(i[2])
             #print(self.chords[len(self.chords)-1])
             index=0
+            print("i got here")
             while((ct-(self.span-i[0]))<self.times[len(self.chords)-1-index]):
                 index+=1
+                #print("i got here 4")
             if ((i[2]==self.chords[len(self.chords)-1] and index==1)):
                 if index==1:
                     self.times[len(self.chords)-1]=ct-(self.span-i[1])
+                    #print("i got here 3")
                 #print(i[2])
             elif(index==0):
+                #print("i got here 2")
                 #print(i)
                 self.tx(i[2])
                 self.chords.append(i[2])
@@ -125,16 +132,19 @@ class record:
         self.lt=ct
 
 
-def listener(): 
+
+def main(rec):
     for i in range(1):
         #enclosedthread1()
-        threads.append(Process(target=enclosedthread, args=(chordsls, 3)))
+        threads.append(Process(target=enclosedthread, args=(chordsls, span)))
         threads[i].start()
         time.sleep(.1)
-      #  send_data.closePort()
-      #  send_data.initialize()
-      #  send_data.sendChord(actual[0]) ------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
-    rec = record() 
+        send_data.closePort()
+        send_data.initialize()
+        send_data.sendChord(actual[0])# ------------TEMPORARILY COMMENTING OUT TO TEST INTEMPO MODE -----------
+        #websocket_endpoint()
+     
+
     while True:
         if breakfree == True: break
         #time.sleep(.01)
@@ -145,19 +155,19 @@ def listener():
                     # print(start_time)
                     newchord=(chordsls.get())
                     #print("Chord detected:")
-                    print(newchord[0])
-                    print("time")
+                    #print(newchord[0])
+                    #print("time")
                     # print(newchord[1].timestamp())
                     #temp.tempoMain(newchord) #TEMPO MODE
-                    rec.manage(newchord) 
+                    rec.manage(newchord) # sends chords to be checked
         except:
             print("empty")
             audio.terminate()
 
-import threading
 @app.on_event("startup")
 async def startlistening():
     loop = asyncio.get_running_loop()
-    threading.Thread(target=listener, daemon=True).start()
+    rec = record()
+    loop.create_task(rxu(rec))
+    threading.Thread(target=main(rec), daemon=True).start()
 
-    
