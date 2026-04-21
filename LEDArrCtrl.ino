ddfaf7de86fab3505d4527d0f5319100231709aa
sublimebovine <87881310+sublimebovine@users.noreply.github.com>
2026-04-21 11:20:11 -0400

    Update LEDArrCtrl.ino

--- LEDArrCtrl.ino
+++ LEDArrCtrl.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -8,19 +8,25 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 144
+#define LED_COUNT 72
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+String InBytes;
 // chord tables
-byte Abstr[2][6] = {
-  {0,0,1,1,1,0},
-  {0,0,0,0,0,1}
+byte AbStr[3][6] = {
+  {0,0,0,0,1,1},
+  {0,0,0,1,0,0},
+  {0,0,1,0,0,0}
 };
 byte FmStr[2][6] = {
   {1,1,1,1,1,1},
   {0,1,1,0,0,0}
+};
+byte FStr[3][6] = {
+  {1,1,1,1,1,1},
+  {0,1,1,0,0,0},
+  {0,0,0,1,0,0}
 };
 byte EbStr[3][6] = {
   {0,0,1,0,0,0},
@@ -56,10 +62,12 @@ byte DmStr[3][6] = {
   {0,0,0,1,0,0},
   {0,0,0,0,1,0}
 };
-byte FrtList [11][3] = {
-  {1,4,0}, // Ab
+
+
+byte FrtList [12][3] = {
+  {3,4,5}, // Ab
   {1,3,0}, // Fm
-  {1,3,4}, //Eb
+  {1,3,4}, // Eb
   {2,3,0}, // G
   {1,2,3}, // C
   {2,3,0}, // D
@@ -67,7 +75,8 @@ byte FrtList [11][3] = {
   {2,0,0}, // A
   {1,2,0}, // Am
   {2,0,0}, // Em
-  {1,2,3} // Dm
+  {1,2,3}, // Dm
+  {1,3,2} // F
 };
 
 void setup() {
@@ -80,24 +89,68 @@ void setup() {
 void loop() {
 byte astrin[3] = {0,0,0};
 byte emstrin[3] = {0,0,0};
-byte ebstrin[3] = {0,0,0};
 byte dmstrin[3] = {0,0,0};
 byte cstrin[3] = {0,0,0};
-Serial.println("assign chord indexes");
-for(int i=0; i < 3; i++) {
-  astrin[i] = FrtList[7][i];
-  Serial.println(astrin[i]);
-  delay(1000);
-  emstrin[i] = FrtList[9][i];
-  Serial.println(emstrin[i]);
-  delay(1000);
-  dmstrin[i] = FrtList[10][i];
-  cstrin[i] = FrtList[4][i];
-  ebstrin[i] = FrtList[2][i];
-}
-Serial.println("chord indices finished");
-chordAddr(ebstrin, EbStr);
 
+if (Serial.available()>0) {
+  InBytes = Serial.readStringUntil('\n');
+  int chordInt = 0;
+  if (InBytes == NULL) {
+    digitalWrite(LED_BUILTIN,LOW);
+    Serial.write("Led off, chord detected:");
+    strip.clear();
+    strip.show();
+  }
+  else {
+    digitalWrite(LED_BUILTIN,HIGH);
+    Serial.write("Led on, chord detected:");
+    chordInt = InBytes.toInt(); 
+  }
+  switch (chordInt) {
+    case 0:
+      strip.clear();
+      strip.show();
+      break;
+    case 1:
+      chordAddr(FrtList[0], AbStr);
+      break;
+    case 2:
+      chordAddr(FrtList[1], FmStr);
+      break;
+    case 3:
+      chordAddr(FrtList[2], EbStr);
+      break;
+    case 4:
+      chordAddr(FrtList[3], GStr);
+      break;
+    case 5:
+      chordAddr(FrtList[4], CStr);
+      break;
+    case 6:
+      chordAddr(FrtList[5], DStr);
+      break;
+    case 7:
+      chordAddr(FrtList[6], EStr);
+      break;
+    case 8:
+      chordAddr(FrtList[7], AStr);
+      break;
+    case 9:
+      chordAddr(FrtList[8], AmStr);
+      break;
+    case 10:
+      chordAddr(FrtList[9], EmStr);
+      break;
+    case 11:
+      chordAddr(FrtList[10], DmStr);
+      break;
+    case 12:
+      chordAddr(FrtList[11], FStr);
+      break;
+  }
+  //strip.show()
+
+}
 }
 
 void chordAddr(byte chord[], byte strings[][6]) { //  'b' counts from 0 to 2...
@@ -112,10 +165,10 @@ void chordAddr(byte chord[], byte strings[][6]) { //  'b' counts from 0 to 2...
     if(chord[i] != 0) {
       for(int s=0; s<6; s++) {
         if(strings[i][s] == 1) {
-          pixel = (6*(chord[i]-1)) + s//s*(chord[i]%2)+(6-s-1)*((chord[i]+1)%2);
+          pixel = ((6*((12-chord[i]))) +(5-s));
           Serial.println(pixel);
           Serial.println(chord[i]);
-          switch (s) { //*(chord[i]%2)+(6-s-1)*((chord[i]+1)%2)
+          switch (s) {
           case 0:
               r=255;
               g=0;
@@ -158,3 +211,6 @@ void chordAddr(byte chord[], byte strings[][6]) { //  'b' counts from 0 to 2...
     }
   }
 }
+
+
+
